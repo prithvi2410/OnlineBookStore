f523d50a3d0c10b225203dc905aa10c9a00dd454
prithviraj <prithvi2410rj@gmail.com>
2022-01-28 13:15:11 +0530

    added profile controller

---
+++ Online Book Store/Controllers/ProfileController.cs
@@ -0,0 +1,26 @@
+﻿using Online_Book_Store.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online_Book_Store.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private OnlineBookStoreContext database = new OnlineBookStoreContext();
+        // GET: Profile
+        public ActionResult Details()
+        {
+            if (TempData["CurrentUserId"] == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TempData.Keep();
+            Customer customer = database.Customers.Find((int)TempData["CurrentUserId"]);
+            return View(customer);
+        }
+    }
+}
